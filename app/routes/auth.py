3ecb352b9cb2af14fa1f315ce096486ffcdee040
dvrkdev <dvrk.devx@gmail.com>
2026-02-01 10:25:58 +0500

    fix: current user checker

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,13 +1,17 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from app.forms import LoginForm
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_user
 from werkzeug.security import check_password_hash
-from flask_login import login_user
+
+from app.forms import LoginForm
+from app.models import User
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 @bp.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("portfolio.index"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -18,4 +22,4 @@ def login():
             return redirect(redirect_url)
         else:
             flash("Invalid username or password", "danger")
-    return render_template("login.html", form=form)
+    return render_template("auth/login.html", form=form)
